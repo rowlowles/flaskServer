869402d08bfccd550383a13ca71309aa14b73b26
Rowlowles <robbie.windsor+git@gmail.com>
2019-01-31 23:26:45 -0500

    RL Did a big ol refactor. Not as clean, but it is simpler and it is working, and with pressing deadlines, working is more impoartnat than clean for this FYDP

--- flaskAPI.py
+++ flaskAPI.py
@@ -1,49 +1,65 @@
-from flask import Flask, request
-from flask_restplus import Resource, Api, fields
-from database import db_session
-from models import cardInfo
+from flask import Flask, request, jsonify
+import mysql.connector
+import decimal
+import json
+
+secrets = json.load(open('secrets.json', encoding="utf8"))
+teamMarfDB = mysql.connector.connect(
+    host=secrets['host'],
+    port=secrets['port'],
+    user=secrets['user'],
+    password=secrets['password'],
+    db=secrets['db']
+)
+curse = teamMarfDB.cursor()
 
 app = Flask(__name__)
-api = Api(app,
-          version='0.1',
-          title='cardobot API',
-          description='Simple API for the cardobot sorter'
-          )
+
+columns = ['setName', 'cardName', 'foil', 'blue', 'green', 'red', 'white', 'black', 'cardPriceUSD', 'foilPrice']
+colours = ['blue', 'green', 'red', 'white', 'black']
+skipColumns = ['setName', 'cardName'] + colours
+colString = ', '.join(columns)
+placeHolders = ', '.join(['%s']*len(columns))
+
+def parseReturnValues(results, cardName):
+    cardList = {"type": 1, "cardInfo": {"cardName": cardName, "sets": []}, "sets": {}}
+
+    for index, result in enumerate(results):
+        setNameIndex = columns.index('setName')
+        setName = result[setNameIndex]
+        cardList["cardInfo"]["sets"].append(setName)
+        cardObj = {}
+        
+        if index == 0:
+            # Get index for the colours
+            # Append colours to parent dictionary
+            for colour in colours:
+                colourVal = result[columns.index(colour)]
+                cardList["cardInfo"][colour] = colourVal
+
+        for item, key in zip(result, columns):
+            if key in skipColumns:
+                continue
+            if type(item) == decimal.Decimal:
+                item = float(item)
+            cardObj[key] = item
+        cardList["sets"][setName] = cardObj
+
+    return jsonify(cardList)
+
+@app.route('/welcome')
+def BaseRoute():
+    return jsonify({'Message': 'Welcome to the cardobot API!'})
 
 
-@api.route('/welcome')
-class BaseRoute(Resource):
-    def get(self):
-        return {'Message': 'Welcome to the cardobot API!'}
-
-
-@api.route('/cardInfo')
-class cardInfoRoute(Resource):
-    model = api.model('Model', {
-        'cardIDNumber': fields.String,
-        'cardName': fields.String,
-        'setName': fields.String,
-        'typeLine': fields.String,
-        'blue': fields.Integer,
-        'green': fields.Integer,
-        'white': fields.Integer,
-        'black': fields.Integer,
-        'red': fields.Integer,
-        'cardPriceUSD': fields.Integer,
-        'lastUpdated': fields.DateTime,
-        'foilPrice': fields.Integer,
-        'tcgIdNumber': fields.Integer
-        })
-
-    @api.marshal_with(model, envelope='resource')
-    def get(self, **kwargs):
-        result = cardInfo.query.all()
-        return result
-
-
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-    db_session.remove()
+@app.route('/cardInfo', methods=['GET', 'POST'])
+def cardInfoRoute():
+    cardName = request.get_json()['cardName']
+    query = 'SELECT %s FROM magicCards WHERE cardName = \"%s\";' % (colString, cardName)
+    print(query)
+    curse.execute(query, cardName)
+    results = curse.fetchall()
+    return parseReturnValues(results, cardName)
 
 
 if __name__ == "__main__":
