7202876c0ca3e8e503c36b00753c73ef810bc81d
Rowlowles <robbie.windsor+git@gmail.com>
2019-02-07 11:53:07 -0500

    RL Added the main commands route.

--- flaskAPI.py
+++ flaskAPI.py
@@ -21,7 +21,7 @@ skipColumns = ['setName', 'cardName'] + colours
 colString = ', '.join(columns)
 placeHolders = ', '.join(['%s']*len(columns))
 
-def parseReturnValues(results, cardName):
+def parseCardReturnValues(results, cardName):
     cardList = {"type": 1, "cardInfo": {"cardName": cardName, "sets": []}, "sets": {}}
 
     for index, result in enumerate(results):
@@ -29,7 +29,7 @@ def parseReturnValues(results, cardName):
         setName = result[setNameIndex]
         cardList["cardInfo"]["sets"].append(setName)
         cardObj = {}
-        
+
         if index == 0:
             # Get index for the colours
             # Append colours to parent dictionary
@@ -47,6 +47,23 @@ def parseReturnValues(results, cardName):
 
     return jsonify(cardList)
 
+
+def parseCommandReturnValues(results):
+    returnObject = []
+    for command in results:
+        commandObject = {'type': 0, 'data': {'sortType': None, 'numCat': None, 'categories': None}}
+        commandObject['data']['sortType'] = command[1]
+        commandObject['data']['numCat'] = command[2]
+        commandObject['data']['categories'] = json.loads(command[3])['categories']
+        returnObject.append(commandObject)
+
+    return jsonify(returnObject)
+
+@app.route('/')
+def hello():
+    return 'Welcome to the cardobot server!'
+
+
 @app.route('/welcome')
 def BaseRoute():
     return jsonify({'Message': 'Welcome to the cardobot API!'})
@@ -56,10 +73,21 @@ def BaseRoute():
 def cardInfoRoute():
     cardName = request.get_json()['cardName']
     query = 'SELECT %s FROM magicCards WHERE cardName = \"%s\";' % (colString, cardName)
-    print(query)
     curse.execute(query, cardName)
     results = curse.fetchall()
-    return parseReturnValues(results, cardName)
+    return parseCardReturnValues(results, cardName)
+
+
+@app.route('/sortCommands', methods=['GET', 'POST'])
+def sortCommandsRoute():
+    debugStat = request.get_json()
+    if 'debug' in debugStat:
+        query = 'SELECT * FROM sortCommands'
+    else:
+        query = 'SELECT * FROM sortCommands WHERE timestamp = (SELECT MAX(timestamp) from sortCommands)'
+    curse.execute(query)
+    results = curse.fetchall()
+    return parseCommandReturnValues(results)
 
 
 if __name__ == "__main__":
